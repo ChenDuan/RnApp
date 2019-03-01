71dc3dc01fd2bb0856b555062b364c268143f855
dchen1 <386223660@qq.com>
2019-03-01 10:01:34 +0800

    跳转

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import WelcomePage from './js/page/WelcomePage';
+import AppNavigator from './js/navigator/AppNavigator'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => AppNavigator);
